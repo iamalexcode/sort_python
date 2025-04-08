06370ef0d52573bbfd892b0f6a1fc27cf06e6840
Alex <110295566+iamalexcode@users.noreply.github.com>
2025-04-08 09:49:10 -0300

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -3,12 +3,10 @@ def merge_sort(vetor):
         meio = len(vetor) // 2
         esquerda = vetor[:meio]
         direita = vetor[meio:]
-
-        
+       
         merge_sort(esquerda)
         merge_sort(direita)
-
-        
+       
         i = j = k = 0
 
         while i < len(esquerda) and j < len(direita):
@@ -19,20 +17,17 @@ def merge_sort(vetor):
                 vetor[k] = direita[j]
                 j += 1
             k += 1
-
         
         while i < len(esquerda):
             vetor[k] = esquerda[i]
             i += 1
             k += 1
-
-        
+     
         while j < len(direita):
             vetor[k] = direita[j]
             j += 1
             k += 1
 
-# Programa principal
 if __name__ == "__main__":
     vetor = []
 
